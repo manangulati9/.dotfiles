ec6b83d55aabf79a6d6a1fbe4479513fc99a9e3e
Manan Gulati <manangulati9@gmail.com>
2024-06-03 15:27:04 +0530

    removed suspend command

--- .local/bin/idle/run.sh
+++ .local/bin/idle/run.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-DIM_TIMEOUT=300
-let LOCK_TIMEOUT=$DIM_TIMEOUT+60
+LOCK_TIMEOUT=480
 let SCREEN_OFF_TIMEOUT=$LOCK_TIMEOUT+120
-let SUSPEND_TIMEOUT=$SCREEN_OFF_TIMEOUT+120
 
 SCRIPT_PATH=~/.local/bin/idle
 
 swayidle -w \
-	timeout $DIM_TIMEOUT "$SCRIPT_PATH/dim.sh" resume "ddcutil setvcp 10 100" \
 	timeout $LOCK_TIMEOUT "$SCRIPT_PATH/autolock.sh" \
-	timeout $SCREEN_OFF_TIMEOUT "$SCRIPT_PATH/screen_off.sh" resume "swaymsg 'output * dpms on'" \
-	timeout $SUSPEND_TIMEOUT "$SCRIPT_PATH/suspend.sh" resume "ddcutil setvcp 10 100; swaymsg 'output * dpms on'" \
+	timeout $SCREEN_OFF_TIMEOUT "$SCRIPT_PATH/screen_off.sh" resume "sleep 1; swaymsg 'output * dpms on'" \
 	before-sleep "$SCRIPT_PATH/autolock.sh"
