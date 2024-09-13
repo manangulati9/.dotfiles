351cc1190b3260a84fc4824e9a1b0a0e80a24048
Manan Gulati <manangulati.dev@gmail.com>
2024-09-13 16:31:22 +0530

    changes

---
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,18 @@
+return {
+  "stevearc/oil.nvim",
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    keymaps = {
+      ["l"] = "actions.select",
+      ["h"] = "actions.parent",
+      ["q"] = "actions.close",
+    },
+    view_options = {
+      show_hidden = true,
+    },
+  },
+  -- Optional dependencies
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+}
