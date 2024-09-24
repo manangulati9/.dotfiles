86da84e1be54a11ab4f10523db43828db55dfa4c
Manan Gulati <manangulati.dev@gmail.com>
2024-09-24 22:04:26 +0530

    changes

--- scripts/.local/share/bin/find-dotfiles.sh
+++ scripts/.local/share/bin/find-dotfiles.sh
@@ -2,16 +2,16 @@
 
 # Check if an argument is provided, if not, use fzf to select a directory from $HOME/Projects
 if [[ $# -eq 1 ]]; then
-	selected=$1
+  selected=$1
 else
-	selected=$(find "$HOME"/.dotfiles -type f | fzf --preview 'bat --style=numbers --color=always {}' --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
-		--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-		--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8)
+  selected=$(find "$HOME"/.dotfiles -type f | fzf --preview 'bat --style=numbers --color=always {}' --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+    --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+    --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8)
 fi
 
 # If no directory is selected, exit
 if [[ -z $selected ]]; then
-	exit 0
+  exit 0
 fi
 
 # Extract the name of the selected directory and replace dots with underscores
@@ -22,8 +22,8 @@ tmux_running=$(pgrep tmux)
 
 # If neither TMUX environment variable nor tmux process is running, start a new tmux session with the selected directory
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-	tmux new-session -s "$selected_name" "nvim $selected"
-	exit 0
+  tmux new-session -s "$selected_name" "nvim $selected"
+  exit 0
 fi
 
 tmux new-window -a -n "$selected_name" "nvim $selected"
