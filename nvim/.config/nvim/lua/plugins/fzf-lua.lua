e1003bba60d55e02821afa7835cfa5909826e59f
Manan Gulati <manang@edpi.ai>
2025-08-19 20:29:29 +0530

    cleanup

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "ibhagwan/fzf-lua",
-  keys = {
-    -- disable the keymap to grep files
-    { "<leader>/", false },
-  },
-}
--- nvim/.config/nvim/lua/plugins/render-markdown.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  opts = {},
-  dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-  -- Install pylatexenc for LateX preview. Use "pip install pylatexenc"
-}
